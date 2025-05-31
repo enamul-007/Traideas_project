40a7f0212d414de3230af2e80b6447952fada875
enamul-007 <anamulkhan523@gmail.com>
2025-05-31 18:32:18 +0600

    new section add

--- src/component/Chose_part/Chose_part.jsx
+++ src/component/Chose_part/Chose_part.jsx
@@ -1,30 +1,55 @@
-import React from 'react'
-import Common_overlay from '../common_component/Common_overlay'
+import React from "react";
+import Common_overlay from "../common_component/Common_overlay";
 
 const Chose_part = () => {
   return (
-    <div  className='bg-black_color'>
-        <div className="container">
-            <div>
-                <div className='pt-[72px]'>
-                    <Common_overlay
-                    title="Why Chose Us"
-                    style={{color:"white"}}
-                    />
-                </div>
-              <div className='pt-9'>
-              <h3 className='text-3xl font-bold font-open_sans text-white_text_color '>
+    <div className="bg-black_color">
+      <div className="container">
+        <div className="py-24">
+          <div className="pt-[72px] ">
+            <Common_overlay title="Why Chose Us" style={{ color: "white" }} />
+          </div>
+          <div className="flex">
+            <div className="pt-9">
+              <h3 className="text-3xl font-bold font-open_sans text-white_text_color ">
                 Heading of your choice
-                </h3>
-                <p className='text-base font-semibold font-open_sans text-white_text_color pt-9 w-[520px] text-justify'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in   
-                </p>
-              </div>
-              
+              </h3>
+              <p className="text-base font-semibold font-open_sans text-white_text_color pt-9 w-[520px] text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in
+              </p>
             </div>
-        </div>
-    </div>
-  )
-}
+            <div className="h-[230px] w-[370px]  ml-[72px] border-[3px] rounded-2xl ">
+            <div className="p-[23px]">
+            <h2 className="text-base font-semibold text-white_text_color font-open_sans">
+                Reason 1
+              </h2>
+              <h1 className="pt-2 text-2xl font-semibold font-open_sans text-white_text_color">
+                Heading
+              </h1>
+              <p className="pt-2 text-base font-semibold text-white_text_color font-open_sans">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. sed
+                do eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+              </p>
+            </div>
+            <div>
+                
 
-export default Chose_part
+
+
+
+                
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chose_part;
